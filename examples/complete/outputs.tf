d8eff9f5913cd47a84198ac9c238feb036e10a4b
Leo Przybylski <leo@cloudposse.com>
2021-09-12 18:09:47 -0700

    feat(module): Adding outputs for testing

---
+++ examples/complete/outputs.tf
@@ -0,0 +1,15 @@
+output "vpn_endpoint_arn" {
+  value       = module.example.vpn_endpoint_arn
+}
+
+output "vpn_endpoint_id" {
+  value       = module.example.vpn_endpoint_id
+}
+ 
+output "vpn_endpoint_dns_name" {
+  value       = module.example.vpn_endpoint_dns_name
+}
+
+output "client_configuration" {
+  value       = module.example.client_configuration
+}
