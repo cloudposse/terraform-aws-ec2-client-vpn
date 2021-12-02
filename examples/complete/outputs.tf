7d01e96d5aeb8af562d9a44239d6292bc5b6a315
nitrocode <nitrocode@users.noreply.github.com>
2021-12-02 14:02:02 -0600

    Use renamed module in outputs

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,16 +1,16 @@
 output "vpn_endpoint_arn" {
-  value = module.example.vpn_endpoint_arn
+  value = module.ec2_client_vpn.vpn_endpoint_arn
 }
 
 output "vpn_endpoint_id" {
-  value = module.example.vpn_endpoint_id
+  value = module.ec2_client_vpn.vpn_endpoint_id
 }
 
 output "vpn_endpoint_dns_name" {
-  value = module.example.vpn_endpoint_dns_name
+  value = module.ec2_client_vpn.vpn_endpoint_dns_name
 }
 
 output "client_configuration" {
   sensitive = true
-  value     = module.example.full_client_configuration
+  value     = module.ec2_client_vpn.full_client_configuration
 }
