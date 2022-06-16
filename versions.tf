c3605d0ebe256c4214ea741a5f2b33a781ec7615
Benjamin Smith <ben.smith.developer@gmail.com>
2022-06-16 10:50:37 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = "~> 3.0"
     }
     awsutils = {
       source  = "cloudposse/awsutils"
-      version = ">= 0.8.0"
+      version = ">= 0.11.0"
     }
   }
 }
