73355e1334d29326e51181f3766287ddcd1fcc21
Nuru <Nuru@users.noreply.github.com>
2023-08-01 02:58:19 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = ">= 4.0"
     }
     awsutils = {
       source  = "cloudposse/awsutils"
-      version = ">= 0.8.0"
+      version = ">= 0.16.0"
     }
   }
 }
